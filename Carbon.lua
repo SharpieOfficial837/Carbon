5ef7592020ab4f2442750a591a185b39aa5b5e2e
jackson <isaiahjcox18@gmail.com>
2026-06-21 17:08:40 +1000

    Delete Carbon.lua

--- Carbon.lua
+++
@@ -1,8 +0,0 @@
-local base = "https://raw.githubusercontent.com/SharpieOfficial837/Carbon/main/Jailbreak/"
-
-local function load(path)
-	return loadstring(game:HttpGet(base .. path))()
-end
-
-local ui = load("ui/new.lua")
-load("modules/606849621/606849621.lua")(ui.Tabs)
